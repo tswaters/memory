a291f72c2c3fae8f70a41c898c553ba938cdbe4f
Tyler Waters <tyler.waters@gmail.com>
2020-02-15 16:19:12 -0800

    npm upgrade, clean up webpack/babel config

--- webpack.config.js
+++ webpack.config.js
@@ -8,92 +8,15 @@ const OfflinePlugin = require('offline-plugin')
 const packageJson = require('./package.json')
 
 module.exports = (env, argv) => {
-  const chunkhash = argv.mode === 'production' ? '.[chunkhash]' : ''
-
-  const rules = [
-    {
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          plugins: [
-            '@babel/plugin-transform-runtime',
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-proposal-object-rest-spread'
-          ],
-          presets: [
-            ['@babel/preset-env', { targets: '> 1%, not dead, not ie < 13' }],
-            '@babel/preset-react'
-          ],
-          sourceMap: true,
-          retainLines: true
-        }
-      }
-    },
-    {
-      test: /\.html$/,
-      loader: 'html-loader'
-    },
-    {
-      test: /\.(css|less)$/,
-      use: [
-        {
-          loader: MiniCssExtractPlugin.loader
-        },
-        {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
-            modules: {
-              localIdentName:
-                argv.mode === 'production'
-                  ? '[hash:base64:5]'
-                  : '[path][name]__[local]--[hash:base64:5]'
-            },
-            localsConvention: 'camelCase',
-            importLoaders: 1
-          }
-        },
-        {
-          loader: 'less-loader',
-          options: {
-            sourceMap: true,
-            relativeUrls: true,
-            noIeCompat: true
-          }
-        }
-      ]
-    }
-  ]
-
-  const plugins = [
-    new webpack.DefinePlugin({
-      'process.env.version': JSON.stringify(packageJson.version)
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/html/index.html',
-      filename: './index.html',
-      minify: {
-        collapseWhitespace: argv.mode === 'production'
-      }
-    }),
-    new MiniCssExtractPlugin({
-      filename: `[name]${chunkhash}.css`,
-      chunkFilename: `[id]${chunkhash}.css`
-    }),
-    new OfflinePlugin({
-      ServiceWorker: {
-        minify: argv.mode === 'production',
-        events: true
-      }
-    })
-  ]
-
+  const isProd = argv.mode === 'production'
+  const chunkhash = isProd ? '.[chunkhash]' : ''
+  const devtool = isProd ? 'hidden-source-map' : 'eval-source-map'
+  const localIdentName = isProd
+    ? '[hash:base64:5]'
+    : '[path][name]__[local]--[hash:base64:5]'
   return {
     name: 'memory',
-    devtool:
-      argv.mode === 'production' ? 'hidden-source-map' : 'eval-source-map',
+    devtool,
     entry: {
       memory: './src/js'
     },
@@ -112,8 +35,62 @@ module.exports = (env, argv) => {
       extensions: ['.webpack.js', '.web.js', '.js', '.jsx', '.less', '.json']
     },
     module: {
-      rules
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.(css|less)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                modules: {
+                  localIdentName
+                },
+                localsConvention: 'camelCase',
+                importLoaders: 1
+              }
+            },
+            {
+              loader: 'less-loader',
+              options: {
+                sourceMap: true,
+                relativeUrls: true,
+                noIeCompat: true
+              }
+            }
+          ]
+        }
+      ]
     },
-    plugins
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.version': JSON.stringify(packageJson.version)
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/html/index.html',
+        filename: './index.html',
+        minify: {
+          collapseWhitespace: isProd
+        }
+      }),
+      new MiniCssExtractPlugin({
+        filename: `[name]${chunkhash}.css`,
+        chunkFilename: `[id]${chunkhash}.css`
+      }),
+      new OfflinePlugin({
+        ServiceWorker: {
+          minify: isProd,
+          events: true
+        }
+      })
+    ]
   }
 }
