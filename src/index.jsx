41b80cd72861e0d377c2acfd45f670eebb818303
Tyler Waters <tyler.waters@gmail.com>
2026-09-22 19:52:10 -0700

    Add a checker for bad emoji widths (non-supported)
    Hopefully this doesn't break anything. In the system I'm
    using all the emojis are returning the same width so I'm
    using the CA flag here as a baseline width and removing
    anything that isn't that size. At the very least, if this
    bit of code removes everything, it should leave the flags
    (and, at the very least, the ca flag)

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,37 @@ import { render } from 'react-dom'
 import App from './App.jsx'
 import { HighScoresProvider } from './HighScores.jsx'
 import { darkMode, lightMode } from './index.css'
+import * as tilesets from './data/index.js'
 
 const theme = window.localStorage.getItem('THEME')
 if (theme) document.body.classList.add(theme === 'DARK' ? darkMode : lightMode)
+
+const testHeight = 32
+const canvas = document.createElement('canvas')
+canvas.width = 54
+canvas.height = testHeight
+
+const ctx = canvas.getContext('2d')
+ctx.fillStyle = '#000'
+ctx.font = `${testHeight}px system`
+
+const removed = []
+const idealWidth = ctx.measureText('🇨🇦').width
+
+for (const [key, value] of Object.entries(tilesets)) {
+  for (let i = value.length - 1; i >= 0; i--) {
+    if (ctx.measureText(value[i].emoji).width !== idealWidth) {
+      console.log(`removed ${value[i].emoji} from ${key}`)
+      removed.push(...value.splice(i, 1))
+    }
+  }
+}
+
+if (removed.length > 0) {
+  console.log(
+    `Removed ${removed.length} glyphs with non-expected width (${idealWidth})`,
+  )
+}
 
 render(
   <HighScoresProvider>
